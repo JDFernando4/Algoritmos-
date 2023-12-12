e168a4a3b70632a24379ea22562d6e454cf914ac
JDFernando4 <151822303+JDFernando4@users.noreply.github.com>
2023-12-12 11:38:17 -0500

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,51 @@
+#El proposito general de esta actividad es aprender a manejar las variables de
+#python y los comandos ya declarados
+#Suma Resta Multiplicacion y Division
+print("#El proposito general de esta actividad es aprender a manejar las variables de")
+print("#python y los comandos ya declarados")
+print("#Suma Resta Multiplicacion y Division")
+a=1
+b=2
+c=a+b
+print(c)
+
+a=4.5
+b=1.2
+c=a-b
+print(c)
+
+a=3
+b=10
+c=a*b
+print(c)
+
+a=10
+b=2
+c=a/b
+print(c)
+print("Suma")
+#resultados de los ejercicios
+print("resultado de los ejercicios")
+
+#variable tipo string que contenga su nombre
+nombre= ("Juan Fernando Quintuña Sucuzhañay")
+print(nombre)
+
+len(nombre)
+caracteres=len(nombre)
+print(caracteres)
+
+#el proposito de este programa es manejar una variable str y contar caracteres con len
+print("el proposito de este programa es manejar una variable str y contar caracteres con len")
+
+#Declara una variable entera y otra punto flotante y hacer la conversion en ambas
+print("Declara una variable entera y otra tipo flotante y hacer la conversion en ambas")
+
+entera= 12
+flotante=1.2
+
+print(entera)
+print(float(entera))
+
+print(flotante)
+print(int(flotante))
